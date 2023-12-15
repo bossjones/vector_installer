ea13092576b5d6de97608ca0b42c4039ca36d066
Malcolm Jones <bossjones@theblacktonystark.com>
2023-12-14 20:56:49 -0500

    chg: dedupe

--- update-systemd-and-config.sh
+++ update-systemd-and-config.sh
@@ -25,13 +25,13 @@ Group=root
 StandardOutput=journal+console
 StandardError=inherit
 ExecStartPre=/usr/local/bin/vector --config /etc/vector/vector.yaml validate
-ExecStart=/usr/local/bin/vector --watch-config --verbose --config /etc/vector/vector.yaml
+ExecStart=/usr/local/bin/vector --watch-config --config /etc/vector/vector.yaml
 ExecReload=/usr/local/bin/vector --config /etc/vector/vector.yaml validate
 ExecReload=/bin/kill -HUP \$MAINPID
 Restart=always
 AmbientCapabilities=CAP_NET_BIND_SERVICE
 EnvironmentFile=-/etc/default/vector
-Environment=VECTOR_LOG=debug
+# Environment=VECTOR_LOG=debug
 # Since systemd 229, should be in [Unit] but in order to support systemd <229,
 # it is also supported to have it here.
 StartLimitInterval=10
